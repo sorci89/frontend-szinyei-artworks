de56ba9e249e1c0cccb990c01c19c7f0b3b7174f
Flora <laszlo.t.flora@gmail.com>
2022-02-14 23:12:17 +0100

    get department

--- src/App.js
+++ src/App.js
@@ -1,7 +1,34 @@
 import './App.css';
+import { useState, useEffect } from 'react';
+
+const axios = require('axios');
 
 function App() {
-  return <div></div>;
+  const [data, setData] = useState('');
+  const [dataList, setDataList] = useState([]);
+  const [department, setDepartment] = useState([]);
+
+  const getDatas = async () => {
+    const response = await axios.get(
+      'https://collectionapi.metmuseum.org/public/collection/v1/objects?metadataDate=2022-02-01'
+    );
+    setDataList(response.data);
+    console.log('data', dataList);
+  };
+  const getDepartment = async () => {
+    const response = await axios.get(
+      'https://collectionapi.metmuseum.org/public/collection/v1/departments'
+    );
+    setDepartment(response.data);
+    console.log(department);
+  };
+
+  useEffect(() => {
+    getDatas();
+    getDepartment();
+  }, []);
+
+  return <div>HELLO MET</div>;
 }
 
 export default App;
