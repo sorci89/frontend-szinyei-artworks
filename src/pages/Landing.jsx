741fe04a6f9f8283757381ece7305708200bd424
SalvadorLimones <87721843+SalvadorLimones@users.noreply.github.com>
2022-02-16 00:19:56 +0100

    add hirst demo

--- src/pages/Landing.jsx
+++ src/pages/Landing.jsx
@@ -1,13 +1,26 @@
-import React from "react";
+import React, { useState } from "react";
 import { Link } from "react-router-dom";
+import Hirst from "../components/Hirst";
 
 const Landing = () => {
+  const [next, swithNext] = useState(false);
+
+  setTimeout(() => {
+    swithNext(true);
+  }, 4000);
+
   return (
-    <Link to="/browser">
-      <div className="login-dot">
-        <p>THE MET</p> <p>image browser</p>
-      </div>
-    </Link>
+    <div className="hirst-container-container">
+      {!next ? (
+        <Link to="/browser">
+          <div className="login-dot">
+            <p>THE MET</p> <p>image browser</p>
+          </div>
+        </Link>
+      ) : (
+        <Hirst />
+      )}
+    </div>
   );
 };
 
