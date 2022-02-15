f56b0c45283b7c6246d0a95e3707d818368787c7
Flora <laszlo.t.flora@gmail.com>
2022-02-15 18:38:52 +0100

    getData

--- src/App.js
+++ src/App.js
@@ -9,33 +9,32 @@ function App() {
   const [department, setDepartment] = useState([]);
   const [search, setSearch] = useState([]);
 
-  //   const getDatas = async () => {
-  //     const response = await axios.get(
-  //       'https://collectionapi.metmuseum.org/public/collection/v1/objects/?objectID=437133'
-  //     );
-  //     setDataList(response.data);
-  //     console.log('data', dataList);
-  //   };
-  //   const getDepartment = async () => {
-  //     const response = await axios.get(
-  //       'https://collectionapi.metmuseum.org/public/collection/v1/departments'
-  //     );
-  //     setDepartment(response.data);
-  //     console.log(department);
-  //   };
+  const getDatas = async () => {
+    const response = await axios.get(
+      'https://api.harvardartmuseums.org/image?apikey=a3ea6ad7-34cb-4507-a771-21efbcd15181&q=name:18th century BCE'
+    );
+    setDataList(response.data);
+    console.log('data', dataList);
+  };
+  // const getDepartment = async () => {
+  //   const response = await axios.get(
+  //     'https://collectionapi.metmuseum.org/public/collection/v1/departments'
+  //   );
+  //   setDepartment(response.data);
+  //   console.log(department);
+  // };
 
   const getSearch = async () => {
-    const response = await axios.get(
-      'https://collectionapi.metmuseum.org/public/collection/v1/search?q=sunflowers&isHighlight=true'
-    );
+    const response = await axios.get();
     setSearch(response.data);
     console.log(search);
   };
 
   useEffect(() => {
-    // getDatas();
+    getDatas();
+    console.log(dataList);
     // getDepartment();
-    getSearch();
+    // getSearch();
   }, []);
 
   return <div>HELLO MET</div>;
