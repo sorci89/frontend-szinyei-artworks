06c7786cbd49505c3245651ee785dcc313128e46
SalvadorLimones <87721843+SalvadorLimones@users.noreply.github.com>
2022-02-16 21:27:07 +0100

    add keword search; fix  no-image fetch issue

--- src/components/Imagebox.jsx
+++ src/components/Imagebox.jsx
@@ -2,12 +2,18 @@ import React from "react";
 
 const Imagebox = (props) => {
   const data = props.data;
-  /* console.log(data.people[0].name); */
   return (
-    <div key={data.images && data.images[0].baseimageurl} className="color-test">
-      <img src={data.images ? data.images[0].baseimageurl : ""} alt="Picture not available" />
+    <div className="color-test">
+      <img
+        src={data.images && data.images[0] ? data.images[0].baseimageurl : ""}
+        alt="not available"
+      />
       <div>{data.title}</div>
-      {data.people ? <div>{data.people[0].name}</div> : <div>Unknown Artist</div>}
+      {data.people ? (
+        <div>{data.people[0].name}</div>
+      ) : (
+        <div>Unknown Artist</div>
+      )}
     </div>
   );
 };
--- src/pages/Browser.jsx
+++ src/pages/Browser.jsx
@@ -8,24 +8,28 @@ const Browser = () => {
   const [dataList, setDataList] = useState([]);
   const [page, setPage] = useState(1);
   const [numberOfPages, setNumberOfPages] = useState(10);
+  const [searchKeyword, updateSearchKeword] = useState("");
+  const [searchCulture, updateSearchCulture] = useState("");
 
   useEffect(() => {
     renderData();
     console.log(page);
-  }, [page]);
+  }, [page, searchKeyword, searchCulture]);
 
   const renderData = async () => {
     let resp = await axios.get(
       /*  "https://api.harvardartmuseums.org/object?size=12&page=10&apikey=a3ea6ad7-34cb-4507-a771-21efbcd15181" */
       // `https://api.harvardartmuseums.org/object?size=12&page=1&classification=Paintings&century=19th%20century&apikey=a8d819ad-b52c-4acb-97b5-88541077022b&size=100`
       // `https://api.harvardartmuseums.org/object?size=12&size=100&page=${pageNumber}&classification=Paintings&century=19th%20century&apikey=a8d819ad-b52c-4acb-97b5-88541077022b`);
-      `https://api.harvardartmuseums.org/object?size=12&size=100&page=${page}&classification=Paintings&century=19th%20century&apikey=a8d819ad-b52c-4acb-97b5-88541077022b`
+      //`https://api.harvardartmuseums.org/object?size=12&size=100&page=${page}&classification=Paintings&century=19th%20century&apikey=a8d819ad-b52c-4acb-97b5-88541077022b`
+      `https://api.harvardartmuseums.org/object?size=9&page=${page}&classification=Paintings&keyword=${searchKeyword}&culture=${searchCulture}&apikey=a8d819ad-b52c-4acb-97b5-88541077022b`
     );
     setDataList(resp.data.records);
     console.log(resp.data);
-
     setNumberOfPages(resp.data.info.pages);
   };
+
+  console.log(searchKeyword);
 
   return (
     <div>
@@ -37,16 +41,30 @@ const Browser = () => {
           backgroundImage: `url("/pictures/bg-paper-texture.jpg")`,
         }}
       >
-        <div className="search-bar"></div>
+        <div className="search-bar">
+          <input
+            type="text"
+            value={searchKeyword}
+            placeholder="kewords"
+            onChange={(e) => {
+              updateSearchKeword(e.target.value);
+              setPage(1);
+            }}
+          ></input>
+          {/*           <input
+            type="text"
+            value={searchCulture}
+            placeholder="culture"
+            onChange={(e) => {
+              updateSearchCulture(e.target.value);
+            }}
+          ></input> */}
+        </div>
 
         {dataList.map((data) => (
           <Imagebox data={data} key={Math.floor(Math.random() * 10000)} />
         ))}
       </div>
-      {/* <button
-        onClick={()=> {setPageNumber(pageNumber - 1); renderData(pageNumber); console.log(pageNumber)}}>Previous</button>
-      <button
-        onClick={()=> {setPageNumber(pageNumber + 1); renderData(pageNumber); console.log(pageNumber)}}>Next</button> */}
       <AppPagination setPage={setPage} page={numberOfPages} />
     </div>
   );
