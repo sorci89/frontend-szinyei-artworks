42ba819eb349f07993ca2fb41ec72a84536fb31b
Flora <laszlo.t.flora@gmail.com>
2022-02-17 10:21:50 +0100

    select image

--- src/components/Imagebox.jsx
+++ src/components/Imagebox.jsx
@@ -1,12 +1,18 @@
-import React from "react";
+import React from 'react';
 
 const Imagebox = (props) => {
   const data = props.data;
+  console.log(data.id);
+  const openImage = (image) => {
+    console.log(image);
+  };
   return (
-    <div className="color-test">
+    <div className='color-test'>
       <img
-        src={data.images && data.images[0] ? data.images[0].baseimageurl : ""}
-        alt="not available"
+        style={{ cursor: 'pointer' }}
+        onClick={() => openImage(data.id)}
+        src={data.images && data.images[0] ? data.images[0].baseimageurl : ''}
+        alt='not available'
       />
       <div>{data.title}</div>
       {data.people ? (
