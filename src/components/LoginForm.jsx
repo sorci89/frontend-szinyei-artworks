5f3eced12f6ae28e4dea5209693ebb809552bb15
Flora <laszlo.t.flora@gmail.com>
2022-02-18 21:29:20 +0100

    useNavigate in LoginForm

--- src/components/LoginForm.jsx
+++ src/components/LoginForm.jsx
@@ -1,12 +1,15 @@
-import React from "react";
-import { useState, useEffect } from "react";
-import axios from "axios";
+import React from 'react';
+import { useState, useEffect } from 'react';
+import axios from 'axios';
+import { useNavigate } from 'react-router-dom';
 
 const LoginForm = () => {
-  const [authUsername, setAuthUsername] = useState("");
-  const [authPassword, setAuthPassword] = useState("");
+  const [authUsername, setAuthUsername] = useState('');
+  const [authPassword, setAuthPassword] = useState('');
   const [logErrors, setLogErrors] = useState({});
-  const [loggedIn, setLoggedIn] = useState("");
+  const [loggedIn, setLoggedIn] = useState('');
+
+  let navigate = useNavigate();
 
   const loginValidation = () => {
     setLogErrors({});
@@ -15,26 +18,26 @@ const LoginForm = () => {
     logErrors.empty = true;
 
     if (!authUsername) {
-      logErrors.authUsername = "Username is required!";
+      logErrors.authUsername = 'Username is required!';
       logErrors.empty = false;
     } else if (authUsername.length < 5) {
-      logErrors.authUsername = "Username must be more than 5 character!";
+      logErrors.authUsername = 'Username must be more than 5 character!';
       logErrors.empty = false;
     }
 
     if (!authPassword) {
-      logErrors.authPassword = "Password is required!";
+      logErrors.authPassword = 'Password is required!';
       logErrors.empty = false;
     } else if (authPassword.length < 5) {
-      logErrors.authPassword = "Password must be more than 5 character!";
+      logErrors.authPassword = 'Password must be more than 5 character!';
       logErrors.empty = false;
     }
     console.log(logErrors);
     if (!logErrors.empty) {
-      console.log("Hiba");
+      console.log('Hiba');
       setLogErrors(logErrors);
     } else {
-      console.log("ok");
+      console.log('ok');
       login();
     }
   };
@@ -42,73 +45,74 @@ const LoginForm = () => {
   const login = async () => {
     try {
       const response = await axios.post(
-        "http://localhost:3101/api/user/login",
+        'http://localhost:3101/api/user/login',
         {},
         {
           headers: {
-            Authorization: authUsername + "&&&" + authPassword,
+            Authorization: authUsername + '&&&' + authPassword,
           },
         }
       );
-      localStorage.setItem("user", authUsername);
-      localStorage.setItem("pw", authPassword);
-      localStorage.setItem("loggedIn", true);
+      localStorage.setItem('user', authUsername);
+      localStorage.setItem('pw', authPassword);
+      localStorage.setItem('loggedIn', true);
       setLoggedIn(true);
-      console.log("válasz ok");
+      console.log('válasz ok');
     } catch (e) {
-      alert("wrong username/password");
+      alert('wrong username/password');
     }
   };
 
   useEffect(() => {
-    setLoggedIn(localStorage.getItem("loggedIn"));
+    setLoggedIn(localStorage.getItem('loggedIn'));
   }, [loggedIn]);
 
   return (
-    <div className="regstration">
+    <div className='regstration'>
       {!loggedIn ? (
         <div>
           <h1>Login</h1>
           <hr />
           {logErrors.authUsername && (
-            <p className="error">{logErrors.authUsername}</p>
+            <p className='error'>{logErrors.authUsername}</p>
           )}
           <input
-            type="text"
+            type='text'
             onChange={(e) => {
               setAuthUsername(e.target.value);
             }}
             value={authUsername}
-            placeholder="Username"
+            placeholder='Username'
           ></input>
           {logErrors.authPassword && (
-            <p className="error">{logErrors.authPassword}</p>
+            <p className='error'>{logErrors.authPassword}</p>
           )}
           <input
-            type="password"
+            type='password'
             onChange={(e) => {
               setAuthPassword(e.target.value);
             }}
             value={authPassword}
-            placeholder="Password"
+            placeholder='Password'
           ></input>
           <button onClick={(e) => loginValidation()}>Login</button>
         </div>
       ) : (
-        <div>
-          <h1>Hi,you're logged in!</h1>
-          <button
-            onClick={(e) => {
-              setAuthUsername("");
-              setAuthPassword("");
-              localStorage.clear();
-              setLoggedIn(false);
-              console.log(loggedIn);
-            }}
-          >
-            Log out
-          </button>
-        </div>
+        navigate('/browser')
+        // <div>
+        //   <h1>Hi,you're logged in!</h1>
+        //   <button
+        //     onClick={(e) => {
+        //       setAuthUsername("");
+        //       setAuthPassword("");
+        //       localStorage.clear();
+        //       setLoggedIn(false);
+        //       console.log(loggedIn);
+        //     }}
+        //   >
+        //     Log out
+        //   </button>
+        // </div>
       )}
     </div>
   );
