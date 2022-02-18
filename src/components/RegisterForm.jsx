bb49e578192131233a6860fd1711fc2e317703d6
DooZeeR <zsolt.ordas@gmail.com>
2022-02-18 14:29:22 +0100

    Back up

--- src/components/RegisterForm.jsx
+++ src/components/RegisterForm.jsx
@@ -3,11 +3,66 @@ import { useState } from 'react';
 import axios from 'axios';
 
 
+
+
 const RegisterForm = (props) => {
 
   const [username, setUsername] = useState("");
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
+  const [regErrors, setRegErrors] = useState({});
+  const [successful, setSuccessful] = useState("");
+  const [alreadyExist, setAlreadyExist] = useState("");
+
+
+
+const validation = () => {
+  setSuccessful("");
+    let Errors = {};
+    Errors.empty= true;
+
+    if (!username) {
+        Errors.username="Username is required!";
+        Errors.empty= false;
+        setAlreadyExist('');
+    }else if (username.length < 5) {
+        Errors.username="Username must be more than 5 character!";
+        Errors.empty= false;
+        setAlreadyExist('');
+    }
+
+
+
+    if (!email) {
+        Errors.email="Email is required!";
+        Errors.empty= false;
+        setAlreadyExist('');
+    }else if(!/\S+@\S+\.\S+/.test(email)){
+        Errors.email="Email is invalid!";
+        Errors.empty= false;
+        setAlreadyExist('');
+    }
+
+
+
+    if (!password) {
+        Errors.password="Password is required!";
+        Errors.empty= false;
+        setAlreadyExist('');
+    }else if (password.length < 5) {
+        Errors.password="Password must be more than 5 character!";
+        Errors.empty= false;
+        setAlreadyExist('');
+    }
+    console.log(Errors);
+    if (!Errors.empty) {
+      console.log("Hiba");
+      setRegErrors(Errors);
+    }else{
+      console.log("ok");
+      signup();
+    }
+}
 
 
   const signup = async () => {
@@ -16,12 +71,17 @@ const RegisterForm = (props) => {
       setUsername('');
       setPassword('');
       setEmail('');
-      alert('Successful registration');
+      setSuccessful("Successful Sign in!");
+      setRegErrors({});
+      setAlreadyExist('');
     } catch (error) {
       if(error.response.status === 400){
-        alert('missing credentials');
+        alert('Missing datas!');
       }else if (error.response.status === 409){
-        alert('Username or Email already exist');
+        setAlreadyExist('Username or Email already exist');
+        setUsername('');
+        setPassword('');
+        setEmail('');
       }
     }
   }
@@ -32,19 +92,25 @@ const RegisterForm = (props) => {
   return (
     <div className="regstration">
         <h1>Registration</h1>
+        <hr/>
+        {successful && <p className="successful">{successful}</p>}
+        {alreadyExist && <p className="error">{alreadyExist}</p>}
+        {regErrors.username && <p className="error">{regErrors.username}</p>}
         <input  type="text" 
                 onChange={(e) => {setUsername(e.target.value)}} 
                 value={username} 
                 placeholder="Username"></input>
+        {regErrors.email && <p className="error">{regErrors.email}</p>}
         <input  type="email" 
                 onChange={(e) => {setEmail(e.target.value)}} 
                 value={email} 
                 placeholder="Email"></input>
+        {regErrors.password && <p className="error">{regErrors.password}</p>}
         <input  type="password" 
                 onChange={(e) => {setPassword(e.target.value)}} 
                 value={password} 
                 placeholder="Password"></input>
-        <button onClick={(e) => signup()}> SIGN UP!</button>    
+        <button onClick={(e) => validation()}> SIGN UP!</button>    
     </div>
   );
 };
