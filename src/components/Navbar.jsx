ba8179991e6a40efb12e3987014c9d75f3bf67b4
SalvadorLimones <87721843+SalvadorLimones@users.noreply.github.com>
2022-02-24 01:13:46 +0100

    fix isse with menu bar not scrolling on smallscree

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,7 +1,7 @@
-import React, { useState, useEffect } from 'react';
-import { Link } from 'react-router-dom';
-import { useNavigate } from 'react-router-dom';
-import './Navbar.scss';
+import React, { useState, useEffect } from "react";
+import { Link } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
+import "./Navbar.scss";
 
 const Navbar = (props) => {
   const [loggedIn, setLoggedIn] = useState(false);
@@ -32,7 +32,7 @@ const Navbar = (props) => {
   //   }, []);
 
   useEffect(() => {
-    setLoggedIn(localStorage.getItem('loggedIn'));
+    setLoggedIn(localStorage.getItem("loggedIn"));
 
     setActive(active);
   }, []);
@@ -41,41 +41,41 @@ const Navbar = (props) => {
     <div>
       {!loggedIn ? (
         <div>
-          <div className='menu-bar'>
-            <div className='menu-left'>
-              <Link to='/browser'>
+          <div className="menu-bar">
+            <div className="menu-left">
+              <Link to="/browser">
                 <div
                   className={
                     isActive === 1
-                      ? 'menu-item item-1 selected'
-                      : 'menu-item item-1'
+                      ? "menu-item item-1 selected"
+                      : "menu-item item-1"
                   }
                 >
                   <p>BROWSE</p>
                 </div>
               </Link>
             </div>
-            <div className='menu-middle'>
-              <div className='logo'></div>
+            <div className="menu-middle">
+              <div className="logo"></div>
             </div>
-            <div className='menu-right'>
-              <Link to='/login'>
+            <div className="menu-right">
+              <Link to="/login">
                 <div
                   className={
                     isActive === 2
-                      ? 'menu-item item-2 selected'
-                      : 'menu-item item-2'
+                      ? "menu-item item-2 selected"
+                      : "menu-item item-2"
                   }
                 >
                   <p>LOGIN</p>
                 </div>
               </Link>
-              <Link to='/register'>
+              <Link to="/register">
                 <div
                   className={
                     isActive === 3
-                      ? 'menu-item item-3 selected'
-                      : 'menu-item item-3'
+                      ? "menu-item item-3 selected"
+                      : "menu-item item-3"
                   }
                 >
                   <p>REGISTER</p>
@@ -83,53 +83,53 @@ const Navbar = (props) => {
               </Link>
             </div>
           </div>
-          <div className='nav-border'></div>
+          <div className="nav-border"></div>
         </div>
       ) : (
         <div>
-          <div className='menu-bar'>
-            <div className='menu-left'>
-              <Link to='/browser'>
+          <div className="menu-bar">
+            <div className="menu-left">
+              <Link to="/browser">
                 <div
                   className={
                     isActive === 1
-                      ? 'menu-item item-1 selected'
-                      : 'menu-item item-1'
+                      ? "menu-item item-1 selected"
+                      : "menu-item item-1"
                   }
                 >
                   <p>BROWSE</p>
                 </div>
               </Link>
 
-              <Link to='/account'>
+              <Link to="/account">
                 <div
                   className={
                     isActive === 2
-                      ? 'menu-item item-2 selected'
-                      : 'menu-item item-2'
+                      ? "menu-item item-2 selected"
+                      : "menu-item item-2"
                   }
                 >
                   <p>MY GALLERY</p>
                 </div>
               </Link>
             </div>
-            <div className='menu-middle'>
-              <div className='logo'></div>
+            <div className="menu-middle">
+              <div className="logo"></div>
             </div>
-            <div className='menu-right'>
+            <div className="menu-right">
               <button
-                className='menu-item item-3'
+                className="menu-item item-3"
                 onClick={() => {
                   localStorage.clear();
                   setLoggedIn(false);
-                  navigate('/browser');
+                  navigate("/browser");
                 }}
               >
                 <p>LOGOUT</p>
               </button>
             </div>
           </div>
-          <div className='nav-border'></div>
+          <div className="nav-border"></div>
         </div>
       )}
     </div>
